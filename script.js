423741544fb3808ac730c16cf3d39ac58263cdd6
Jason Nguyen <jason.blvck@Jasons-MacBook-Pro.local>
2024-09-02 13:22:38 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const cardHolderElement = document.getElementById('cardHolder');
+const cardExpiryElement = document.getElementById('cardExpiry');
+const cardCVVElement = document.getElementById('cardCVV');
+
+// Update the card holder name
+cardHolderElement.textContent = 'Jason Nguyen';
+
+// Update the card expiry date
+cardExpiryElement.textContent = '01/30';
+
+// Update the card CVV
+cardCVVElement.textContent = '007';
